576f920f63e86685803863338dcaba9ea79e51cd
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2012-08-06 21:06:57 +0100

    refactored the ui & dom stuff

--- src/rocket/player/Player.js
+++ src/rocket/player/Player.js
@@ -13,7 +13,9 @@ goog.exportSymbol('RocketPlayer', rocket.player.Player);
  */
 rocket.player.Player = function (app, userOptions) {
 
-	this.app		= app;
+	this.app = app;
+
+	// load a blank app if none specified
 	if (!this.app)
 		this.app = {
 			'initalise': function () { },
@@ -21,7 +23,7 @@ rocket.player.Player = function (app, userOptions) {
 			'pause': function () { }
 		};
 
-	// default options
+	// set default options
 	this.options	= {
 		'id'			: undefined,
 		'title'			: undefined,
@@ -34,10 +36,10 @@ rocket.player.Player = function (app, userOptions) {
 	// extend default options with user options
 	goog.object.extend(this.options, userOptions);
 
+	this.canvasContext	= undefined;
+	this.ui				= {}; 			/* collection of dom elements */
 
-	this.canvas		= undefined;
-	this.ctx		= undefined;
-	this.running	= false;
+	this.running		= false;
 
 	this.dictionary = {
 		overlayPlay		: '&#9658;',
@@ -48,75 +50,151 @@ rocket.player.Player = function (app, userOptions) {
 
 	this.init();
 
-
 };
 
 rocket.player.Player.prototype = {
 		
 	init: function () {
 
-		this.createCanvas();
-		this.createOverlay();
+		// create the player interface
+		this.createPlayerUi();
 
 		// initalise the application
 		this.app['initalise']({
-			'canvas'			: this.canvas,
-			'canvasCtx'			: this.ctx,
+			'canvas'			: this.ui.canvas,
+			'canvasContext'		: this.canvasContext,
 			'viewportWidth'		: undefined,
 			'viewportHeight'	: undefined,
 			'renderCallback'	: goog.bind(this.appTicked, this)
 		});
 
-		if (window.rocketPlayerAppId === undefined && this.options['autoPlay']) this.play({animateOverlay:false});
+		// check if the application wants to autoplay
+		if (window.rocketPlayerAppId === undefined && this.options['autoPlay']) 
+			this.play({animateOverlay:false});
 
 	},
 
-	createCanvas: function () {
+	play: function (options) {
 
-        this.canvas = document.createElement("canvas");
+		if (this.running) return;
+		if (!options) options = {};
 
-        if (!this.canvas.getContext) {
-//			this.events.fire('LOG',"Failed to create canvas element");
-            throw "Failed to create canvas element";
-        }
+		// start the app running
+		this.app['run']();
+		this.running = true;
 
-        this.ctx = this.canvas.getContext('2d');
+		// set the global app id to current
+		window.rocketPlayerAppId = this.options.id;
+		
+		// update ui
+		this.ui.playPauseButton.innerHTML	= this.dictionary.pauseButton;
+		// hide the overlay - check is should be animated or not
+		if (typeof options.animateOverlay == 'boolean' && options.animateOverlay == false) {
+			goog.style.setOpacity(this.ui.overlay, 0);
+			goog.style.showElement(this.ui.overlay, false);
+		} else {
+			var anim = new goog.fx.dom.FadeOutAndHide(this.ui.overlay, 100);
+			anim.play();
+		}
 
-		this.canvas.id		= this.options.id+'_canvas';
-		this.canvas.width	= this.options.width;
-		this.canvas.height	= this.options.height;
+	},
 
-		this.canvas.style.width = '100%';
-        
-        // add canvas to body
-        var playerContainer = document.getElementById(this.options.id);
+	pause: function (options) {
 
-       goog.style.setStyle(playerContainer, {
+		if (!this.running) return;
+		if (!options) options = {};
+
+		// pause the app
+		this.app['pause']();
+		this.running = false;
+
+		// update ui
+		this.ui.playPauseButton.innerHTML	= this.dictionary.playButton;
+
+		// show overlay - check if should be animated or not
+		if (typeof options.animateOverlay == 'boolean' && options.animateOverlay == false) {
+			goog.style.setOpacity(this.ui.overlay, 1);
+			goog.style.showElement(this.ui.overlay, true);
+		} else {
+			var anim = new goog.fx.dom.FadeInAndShow(this.ui.overlay, 200);
+			anim.play();
+		}
+	},
+
+	toggle: function (options) {
+
+		if (!this.running)
+			this.play(options);
+		else
+			this.pause(options);
+	},
+
+	appTicked: function () {
+		
+		var cycleData = {fps:'?'};
+
+		this.ui.fps.innerHTML = (cycleData.fps?cycleData.fps:'...');
+
+		if (this.running && window.rocketPlayerAppId !== this.options.id) {
+			this.toggle();
+		}
+	},
+
+	createPlayerUi: function () {
+
+		/* Create Player Container */
+        this.ui.playerContainer = goog.dom.createDom('div');
+		goog.style.setStyle(this.ui.playerContainer, {
 			'position'			: 'relative',
 			'backgroundColor'	: '#444',
 			'border'			: '1px solid #111',
-			'maxWidth'			: this.options.width+'px',
+			'maxWidth'			: this.options['width']+'px',
 			'margin'			: '10px auto 10px auto',
 			'borderRadius'		: '0 0 5px 5px'
 		});
 
-        playerContainer.appendChild(this.canvas);
+        // attach container to user referenced dom node
+		var rootContainer = document.getElementById(this.options.id);
+		rootContainer.appendChild(this.ui.playerContainer);
+
+
+
+		/* Create Canvas Element */
+		this.ui.canvas = goog.dom.createDom('canvas', {
+			'id'		: this.options['id']+'_canvas',
+			'width'		: this.options['width'],
+			'height'	: this.options['height']
+		});
+		goog.style.setStyle(this.ui.canvas, {
+			'width'		: '100%'
+		});
+		this.ui.playerContainer.appendChild(this.ui.canvas);
 		
-		//this.events.listen('VIEWPORT:RESIZE', this.resize, this);
-        var controlBar = document.createElement("div");
+
+		/* Save Canvas Context */
+        if (!this.ui.canvas.getContext) {
+        	// handle browser support issue... rollback to flash?
+			// this.events.fire('LOG',"Browser not supported");
+            throw "Failed to create canvas element";
+        }
+		this.canvasContext = this.ui.canvas.getContext('2d');
+        
 
 
-       goog.style.setStyle(controlBar, {
+        /* Create Control Bar */
+		this.ui.controlBar = goog.dom.createDom('div');
+		goog.style.setStyle(this.ui.controlBar, {
 			'backgroundColor'	: '#111',
 			'height'			: '22px',
 			'padding'			: '5px',
 			'textAlign'			: 'left'
 		});
 
-        this.playPause = document.createElement("button");
-		this.playPause.innerHTML = this.dictionary.playButton;
 
-		goog.style.setStyle(this.playPause, {
+		/* Create Play / Pause Button */
+        this.ui.playPauseButton = goog.dom.createDom('button');
+		this.ui.playPauseButton.innerHTML = this.dictionary.playButton;
+		goog.style.setStyle(this.ui.playPauseButton, {
 			'backgroundColor'	: '#ff8000',
 			'color'				: 'rgb(255,255,255)',
 			'borderRadius'		: '5px',
@@ -127,66 +205,63 @@ rocket.player.Player.prototype = {
 			'marginRight'		: '10px',
 			'cursor'			: 'hand'
 		});
-
-		goog.events.listen(this.playPause, 'click', goog.bind(this.toggle, this));
-
-        controlBar.appendChild(this.playPause);
+		goog.events.listen(this.ui.playPauseButton, 'click', goog.bind(this.toggle, this));
+        this.ui.controlBar.appendChild(this.ui.playPauseButton);
 
 
 
-        var playerTitleContainer = document.createElement("span");
-		playerTitleContainer.style.color	= 'rgb(180,180,180)';
-		playerTitleContainer.style.font		= '12px Arial';
-		playerTitleContainer.style.fontWeight		= 'bold';
-
-        var playerTitle = document.createElement("span");
-		playerTitle.innerHTML		= this.options.title.toUpperCase();
-
-		goog.style.setStyle(playerTitle, {
+        /* Application Title */
+        this.ui.playerTitle = goog.dom.createDom('span');
+		this.ui.playerTitle.innerHTML = this.options.title.toUpperCase();
+		goog.style.setStyle(this.ui.playerTitle, {
 			'color'		: '#ff8000',
 			'font'		: '12px Arial',
 			'fontWeight': 'bold'
 		});
 
-		playerTitleContainer.appendChild(document.createTextNode(this.dictionary.appType+': '));
-		playerTitleContainer.appendChild(playerTitle);
-        controlBar.appendChild(playerTitleContainer);
-
-
-
-        playerContainer.appendChild(controlBar);
-
-        var fpsContainer = document.createElement("span");
-		goog.style.setStyle(fpsContainer, {
+		/* Application Title Container */
+		this.ui.playerTitleContainer = goog.dom.createDom('span');
+		goog.style.setStyle(this.ui.playerTitleContainer, {
 			'color'		: 'rgb(180,180,180)',
 			'font'		: '12px Arial',
-			'float'		: 'right'
+			'fontWeight': 'bold'
 		});
+		this.ui.playerTitleContainer.appendChild(document.createTextNode(this.dictionary.appType+': '));
+		this.ui.playerTitleContainer.appendChild(this.ui.playerTitle);
+        this.ui.controlBar.appendChild(this.ui.playerTitleContainer);
 
-        this.fps = document.createElement("span");
-		this.fps.innerHTML				= '...';
-		goog.style.setStyle(this.fps, {
+
+
+		/* Create FPS span */
+        this.ui.fps = goog.dom.createDom('span');
+		this.ui.fps.innerHTML				= '...';
+		goog.style.setStyle(this.ui.fps, {
 			'color'			: '#ff8000',
 			'font'			: '12px Arial',
 			'fontWeight'	: 'bold'
 		});
 
 
-		fpsContainer.appendChild(document.createTextNode('FPS: '));
-		fpsContainer.appendChild(this.fps);
+        /* Create FPS container */
+        this.ui.fpsContainer = goog.dom.createDom('span');
+		goog.style.setStyle(this.ui.fpsContainer, {
+			'color'		: 'rgb(180,180,180)',
+			'font'		: '12px Arial',
+			'float'		: 'right'
+		});
+		this.ui.fpsContainer.appendChild(document.createTextNode('FPS: '));
+		this.ui.fpsContainer.appendChild(this.ui.fps);
+        this.ui.controlBar.appendChild(this.ui.fpsContainer);
 
 
-        controlBar.appendChild(fpsContainer);
-
-
-        playerContainer.appendChild(controlBar);
+        // add control bar to Player
+        this.ui.playerContainer.appendChild(this.ui.controlBar);
 
 
 
-        // description
-        var infoBar = document.createElement("div");
-
-       goog.style.setStyle(infoBar, {
+        /* Create Info Bar (description) */
+        this.ui.infoBar = goog.dom.createDom('div');
+		goog.style.setStyle(this.ui.infoBar, {
 			'backgroundColor'	: '#333',
 			'color'				: '#777',
 			'padding'			: '5px 5px 7px 5px',
@@ -194,74 +269,13 @@ rocket.player.Player.prototype = {
 			'font'				: '12px Arial',
 			'borderRadius'		: '0 0 5px 5px'
 		});
+		this.ui.infoBar.appendChild(document.createTextNode(this.options.description));
+        this.ui.playerContainer.appendChild(this.ui.infoBar);
 
-		infoBar.appendChild(document.createTextNode(this.options.description));
 
-        playerContainer.appendChild(infoBar);
-
-	},
-	play: function (options) {
-
-		if (!this.running) this.toggle(options);
-
-	},
-	toggle: function (options) {
-
-		if (!options) options = {};
-
-		if (!this.running) {
-			
-			/* run the application */
-			this.app['run']();
-			this.running				= true;
-			window.rocketPlayerAppId	= this.options.id;
-			this.playPause.innerHTML	= this.dictionary.pauseButton;
-
-			// hide overlay
-			if (typeof options.animateOverlay == 'boolean' && options.animateOverlay == false) {
-				goog.style.setOpacity(this.overlay, 0);
-				goog.style.showElement(this.overlay, false);
-			} else {
-				var anim = new goog.fx.dom.FadeOutAndHide(this.overlay, 150);
-				anim.play();
-			}
-			
-
-		} else {
-
-			/* Pause the application */
-			this.app['pause']();
-			this.running				= false;
-			this.playPause.innerHTML	= this.dictionary.playButton;
-
-			// show overlay
-			if (typeof options.animateOverlay == 'boolean' && options.animateOverlay == false) {
-				goog.style.setOpacity(this.overlay, 1);
-				goog.style.showElement(this.overlay, true);
-			} else {
-				var anim = new goog.fx.dom.FadeInAndShow(this.overlay, 200);
-				anim.play();
-			}
-		}
-
-	},
-
-	appTicked: function () {
-		
-		var cycleData = {fps:'?'};
-
-		this.fps.innerHTML				= (cycleData.fps?cycleData.fps:'...');
-
-		if (this.running && window.rocketPlayerAppId !== this.options.id) {
-			this.toggle();
-		}
-
-	},
-	createOverlay: function () {
-
-        this.overlay = document.createElement("div");
-
-       goog.style.setStyle(this.overlay, {
+        /* Create Paused Overlay */
+        this.ui.overlay = goog.dom.createDom('div');
+		goog.style.setStyle(this.ui.overlay, {
 			'backgroundColor'	: 'rgba(0,0,0,0.35)',
 			'width'				: '100%',
 			'height'			:  this.options.height+'px',
@@ -270,36 +284,23 @@ rocket.player.Player.prototype = {
 			'left'				: 0,
 			'cursor'			: 'hand'
         });
+		// overlay: play icon
+		this.ui.playButton = goog.dom.createDom('div');
+		this.ui.playButton.innerHTML = this.dictionary.overlayPlay;
+		goog.style.setStyle(this.ui.playButton, {
+			'color'			: '#eee',
+			'fontSize'		: '64px',
+			'textAlign'		: 'center',
+			'lineHeight'	: this.options.height+'px',
+			'margin'		: 'auto auto auto auto',
+			'textShadow'	: '0px 0px 15px #222'
+		});
+		this.ui.overlay.appendChild(this.ui.playButton);
+        this.ui.playerContainer.appendChild(this.ui.overlay);
+		goog.events.listen(this.ui.overlay, 'mousemove', goog.bind(this.play, this));
+		goog.events.listen(this.ui.overlay, 'click', goog.bind(this.play, this));
 
 
-       // playButton image specified?
-		if (this.options.playButton) {
-			this.playButton = document.createElement("img");
-			this.playButton.src = this.options.playButton?this.options.playButton:'img/play.png';
-			this.playButton.margin = 'auto auto auto auto';
-		
-		} else {
-			this.playButton = document.createElement("div");
-			this.playButton.innerHTML = this.dictionary.overlayPlay;
-		
-			goog.style.setStyle(this.overlay, {
-				'color'			: '#eee',
-				'fontSize'		: '64px',
-				'textAlign'		: 'center',
-				'lineHeight'	: this.options.height+'px',
-				'margin'		: 'auto auto auto auto',
-				'textShadow'	: '0px 0px 15px #222'
-			});
-			
-
-		}
-		this.overlay.appendChild(this.playButton);
-
-        var playerContainer = document.getElementById(this.options.id);
-        playerContainer.appendChild(this.overlay);
-
-		goog.events.listen(this.overlay, 'mousemove', goog.bind(this.play, this));
-		goog.events.listen(this.overlay, 'click', goog.bind(this.play, this));
 	}
 
 };
