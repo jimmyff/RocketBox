76437cfcc78c103f9dcc2e9d5d45e99af58cb7ac
Jimmy Forrester-Fellowes <jimmyxx@gmail.com>
2012-08-09 22:48:35 +0200

    Added fullscreen support, needs better styling. Will add application resolution & ratio options to config options. Tried Fullscreen API but decided against using it at this stage. Player resize events are now issued to the applications.

--- src/rocket/rocketbox/RocketBox.js
+++ src/rocket/rocketbox/RocketBox.js
@@ -4,7 +4,11 @@ goog.require('rocket.util.Events');
 goog.require('rocket.util.Timer');
 goog.require('goog.events');
 goog.require('goog.style');
+goog.require('goog.dom');
+goog.require('goog.dom.classes');
 goog.require('goog.object');
+goog.require('goog.dom.ViewportSizeMonitor');
+goog.require('goog.math.Size');
 
 goog.exportSymbol('RocketBox', rocket.rocketbox.RocketBox);
 
@@ -28,7 +32,8 @@ rocket.rocketbox.RocketBox = function (app, userOptions) {
 		'autoPlay'		: true,
 		'fps'			: 30,
 		'width'			: 480,
-		'height'		: 320
+		'height'		: 320,
+		'fullscreen'	: false
 	};
 
 	// extend default options with user options
@@ -38,16 +43,21 @@ rocket.rocketbox.RocketBox = function (app, userOptions) {
 	this.ui				= {};			/* collection of dom elements */
 
 	this.dictionary = {
-		overlayPlay		: '&#9658;',
-		playButton		: '&#9658; PLAY',
-		pauseButton		: '&#10073;&#10073; PAUSE',
-		appType			: 'DEMO'
+		overlayPlay			: '&#9658;',
+		playButton			: '&#9658; PLAY',
+		pauseButton			: '&#10073;&#10073; PAUSE',
+		appType				: 'DEMO',
+		fullscreenButton	: 'FULLSCREEN'
 	};
 	
 	this.eventDictionary = {
 		appTick			: 'APP:TICK',
 		appPlay			: 'APP:PLAY',
 		appPause		: 'APP:PAUSE'
+	};
+
+	this.cssDictionary = {
+		base:'.rocketBox.fullscreen { margin:0 !important; max-width:100% !important; z-index:5337; position:absolute !important; width:100% !important; height:100% !important; left:0px; right:0px;  top:0px; bottom:0px; } .rocketBoxFullScreenBody { width:100%; height:100%; margin:0; }'
 	};
 
 	// create the event handler
@@ -60,18 +70,35 @@ rocket.rocketbox.RocketBox = function (app, userOptions) {
 		tickEvent	: this.eventDictionary.appTick
 	});
 
+	// size of the canvas the app knows about
+    this.processedCanvasSize = new goog.math.Size (this.options['width'], this.options['height']);
+
+    // monitor changes in viewport size
+	this.viewportSizeMonitor = new goog.dom.ViewportSizeMonitor();
+    goog.events.listen(
+		this.viewportSizeMonitor, 
+		goog.events.EventType.RESIZE, 
+		goog.bind(this.possibleCanvasResized, this)
+    );
+
 	this.init();
 
 };
 
 rocket.rocketbox.RocketBox.prototype = {
-		
+	
+
 	init: function () {
+
+		goog.style.installStyles(this.cssDictionary.base);
 
 		this.events.listen('APP:TICK', this.appTicked, this);
 
 		// create the player interface
 		this.createPlayerUi();
+
+		// check size
+		this.possibleCanvasResized();
 
 		// initalise the application
 		this.app['initalise']({
@@ -92,7 +119,56 @@ rocket.rocketbox.RocketBox.prototype = {
 
 	},
 
-	toggle: function () {
+	possibleCanvasResized: function () {
+
+		var canvasSize = goog.style.getSize(this.ui.canvas);
+		
+		if (goog.math.Size.equals(this.processedCanvasSize, canvasSize))
+			return;
+		
+		// update the overlay
+		goog.style.setStyle(this.ui.overlay, {
+			'width'		: '100%',
+			'height'	: canvasSize.height+'px'
+		});
+		goog.style.setStyle(this.ui.overlayPlayButton, {
+			'lineHeight'	: canvasSize.height+'px'
+		});
+
+
+		this.processedCanvasSize = canvasSize;
+
+		this.events.fire('APP:RESIZE', {
+			'width'		: canvasSize.width,
+			'height'	: canvasSize.height
+		});
+		
+		goog.dom.setProperties(this.ui.canvas, {
+			'width'		: canvasSize.width,
+			'height'	: canvasSize.height
+		});
+
+
+		
+	},
+
+	toggleFullScreen: function () {
+
+		if (goog.dom.classes.has(this.ui.playerContainer, 'fullscreen')) {
+			goog.dom.classes.remove(this.ui.playerContainer, 'fullscreen');
+			goog.dom.classes.remove(document.body, 'rocketBoxFullScreenBody');
+
+
+		} else {
+			goog.dom.classes.add(this.ui.playerContainer, 'fullscreen');
+			goog.dom.classes.add(document.body, 'rocketBoxFullScreenBody');
+		}
+
+		this.possibleCanvasResized();
+
+	},
+
+	togglePlayPause: function () {
 	
 		if (!this.timer.running) this.play();
 		else this.pause();
@@ -141,7 +217,9 @@ rocket.rocketbox.RocketBox.prototype = {
 	createPlayerUi: function () {
 
 		/* Create Player Container */
-		this.ui.playerContainer = goog.dom.createDom('div');
+		this.ui.playerContainer = goog.dom.createDom('div', {
+			'class'		: 'rocketBox'
+		});
 		goog.style.setStyle(this.ui.playerContainer, {
 			'position'			: 'relative',
 			'backgroundColor'	: '#444',
@@ -183,7 +261,7 @@ rocket.rocketbox.RocketBox.prototype = {
 		this.ui.controlBar = goog.dom.createDom('div');
 		goog.style.setStyle(this.ui.controlBar, {
 			'backgroundColor'	: '#111',
-			'height'			: '22px',
+			'height'			: '25px',
 			'padding'			: '5px',
 			'textAlign'			: 'left'
 		});
@@ -201,10 +279,31 @@ rocket.rocketbox.RocketBox.prototype = {
 			'font'				: '12px Arial',
 			'fontWeight'		: 'bold',
 			'marginRight'		: '10px',
-			'cursor'			: 'hand'
+			'cursor'			: 'hand',
+			'height'			: '25px'
 		});
-		goog.events.listen(this.ui.playPauseButton, 'click', goog.bind(this.toggle, this));
+		goog.events.listen(this.ui.playPauseButton, 'click', goog.bind(this.togglePlayPause, this));
 		this.ui.controlBar.appendChild(this.ui.playPauseButton);
+
+
+		/* Create Fullscreen Button */
+		this.ui.fullscreenButton = goog.dom.createDom('button');
+		this.ui.fullscreenButton.innerHTML = this.dictionary.fullscreenButton.toUpperCase();
+		goog.style.setStyle(this.ui.fullscreenButton, {
+			'backgroundColor'	: '#ff8000',
+			'color'				: 'rgb(255,255,255)',
+			'borderRadius'		: '5px',
+			'border'			: '0px',
+			'width'				: '100px',
+			'font'				: '12px Arial',
+			'fontWeight'		: 'bold',
+			'marginLeft'		: '10px',
+			'float'				: 'right',
+			'cursor'			: 'hand',
+			'height'			: '25px'
+		});
+		goog.events.listen(this.ui.fullscreenButton, 'click', goog.bind(this.toggleFullScreen, this));
+		this.ui.controlBar.appendChild(this.ui.fullscreenButton);
 
 
 
@@ -212,19 +311,22 @@ rocket.rocketbox.RocketBox.prototype = {
 		this.ui.playerTitle = goog.dom.createDom('span');
 		this.ui.playerTitle.innerHTML = this.options['title'].toUpperCase();
 		goog.style.setStyle(this.ui.playerTitle, {
-			'color'		: '#ff8000',
-			'font'		: '12px Arial',
-			'fontWeight': 'bold'
+			'color'			: 'rgb(180,180,180)',
+			'font'			: '12px Arial',
+			'fontWeight'	: 'bold',
+			'lineHeight'	: '25px'
 		});
 
 		/* Application Title Container */
 		this.ui.playerTitleContainer = goog.dom.createDom('span');
 		goog.style.setStyle(this.ui.playerTitleContainer, {
-			'color'		: 'rgb(180,180,180)',
+			'color'		: 'rgb(100,100,100)',
 			'font'		: '12px Arial',
-			'fontWeight': 'bold'
+			'fontWeight': 'bold',
+			'lineHeight'	: '25px'
 		});
-		this.ui.playerTitleContainer.appendChild(document.createTextNode(this.dictionary.appType+': '));
+		if (this.options['appType'])
+			this.ui.playerTitleContainer.appendChild(document.createTextNode(this.options['appType'].toUpperCase()+': '));
 		this.ui.playerTitleContainer.appendChild(this.ui.playerTitle);
 		this.ui.controlBar.appendChild(this.ui.playerTitleContainer);
 
@@ -234,18 +336,20 @@ rocket.rocketbox.RocketBox.prototype = {
 		this.ui.fps = goog.dom.createDom('span');
 		this.ui.fps.innerHTML				= '...';
 		goog.style.setStyle(this.ui.fps, {
-			'color'			: '#ff8000',
+			'color'			: 'rgb(140,140,140)',
 			'font'			: '12px Arial',
-			'fontWeight'	: 'bold'
+			'fontWeight'	: 'bold',
+			'lineHeight'	: '25px'
 		});
 
 
 		/* Create FPS container */
 		this.ui.fpsContainer = goog.dom.createDom('span');
 		goog.style.setStyle(this.ui.fpsContainer, {
-			'color'		: 'rgb(180,180,180)',
+			'color'		: 'rgb(100,100,100)',
 			'font'		: '12px Arial',
-			'float'		: 'right'
+			'float'		: 'right',
+			'lineHeight'	: '25px'
 		});
 		this.ui.fpsContainer.appendChild(document.createTextNode('FPS: '));
 		this.ui.fpsContainer.appendChild(this.ui.fps);
@@ -283,9 +387,9 @@ rocket.rocketbox.RocketBox.prototype = {
 			'cursor'			: 'hand'
 		});
 		// overlay: play icon
-		this.ui.playButton = goog.dom.createDom('div');
-		this.ui.playButton.innerHTML = this.dictionary.overlayPlay;
-		goog.style.setStyle(this.ui.playButton, {
+		this.ui.overlayPlayButton = goog.dom.createDom('div');
+		this.ui.overlayPlayButton.innerHTML = this.dictionary.overlayPlay;
+		goog.style.setStyle(this.ui.overlayPlayButton, {
 			'color'			: '#eee',
 			'fontSize'		: '64px',
 			'textAlign'		: 'center',
@@ -293,11 +397,71 @@ rocket.rocketbox.RocketBox.prototype = {
 			'margin'		: 'auto auto auto auto',
 			'textShadow'	: '0px 0px 15px #222'
 		});
-		this.ui.overlay.appendChild(this.ui.playButton);
+		this.ui.overlay.appendChild(this.ui.overlayPlayButton);
 		this.ui.playerContainer.appendChild(this.ui.overlay);
 		goog.events.listen(this.ui.overlay, 'mousemove', goog.bind(this.play, this));
 		goog.events.listen(this.ui.overlay, 'click', goog.bind(this.play, this));
 
 	}
 
+
+
+/*
+
+	// Below is an implementation of the Fullscreen API
+	// Removed until it's had longer in the oven
+
+	initaliseFullScreen: function () {
+
+		// setup event listeners
+		var fullScreenEnter = goog.bind(this.fullScreenEnter, this);
+		var fullScreenExit = goog.bind(this.fullScreenExit, this);
+		
+		document.addEventListener("fullscreenchange", function () {
+			if (document.fullscreen) fullScreenEnter();
+			else fullScreenExit()
+		}, false);
+
+		document.addEventListener("mozfullscreenchange", function () {
+		    if (document.mozFullScreen)fullScreenEnter();
+			else fullScreenExit()
+		}, false);
+
+		document.addEventListener("webkitfullscreenchange", function () {
+		   if (document.webkitIsFullScreen) fullScreenEnter();
+			else fullScreenExit()
+		}, false);
+	},
+
+	fullScreenEnter: function () {
+
+	},
+
+	fullScreenExit: function () {
+
+			goog.dom.setProperties(this.ui.canvas, {
+				'width':this.options['width'],
+				'height':this.options['height']
+			});
+	},
+
+	startFullScreen: function () {
+
+		goog.dom.classes.add(this.ui.canvas, 'fullscreen');
+
+		if (this.ui.canvas['requestFullscreen']) this.ui.canvas['requestFullscreen']();
+		else if (this.ui.canvas['mozRequestFullScreen']) this.ui.canvas['mozRequestFullScreen']();
+		else if (this.ui.canvas['webkitRequestFullScreen']) this.ui.canvas['webkitRequestFullScreen']();
+		else if (this.ui.canvas['msRequestFullScreen']) this.ui.canvas['msRequestFullScreen']();
+
+		var vpSize = goog.dom.getViewportSize();
+
+		goog.dom.setProperties(this.ui.canvas, {
+			'width':vpSize.width,
+			'height':vpSize.height
+		});
+
+	},
+
+	*/
 };
